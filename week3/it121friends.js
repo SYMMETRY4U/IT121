afc53e3e5949484731be8f7a3e92b98a88d9333d
SYMMETRY4U <121978473+SYMMETRY4U@users.noreply.github.com>
2023-04-20 17:20:14 -0700

    Create it121friends.js

---
+++ week3/it121friends.js
@@ -0,0 +1,18 @@
+const people = {friends: []};
+const friend1 = {
+  firstName: "Cliff",
+  lastName: "Strong",
+  id: 1345
+};
+const friend2 = {
+  firstName: "Melly",
+  lastName: "Strong",
+  id: 6890
+};
+const friend3 = {
+  firstName: "Norfelly",
+  lastName: "Escander",
+  id: 2680
+};
+people.friends.push(friend1, friend2, friend3);
+console.log(people);
